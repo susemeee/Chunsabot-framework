4c7c7cc1f31dc3a8d91fc0b36522b4cb0b1976ff
susemine* <susemeee@gmail.com>
2015-11-22 17:29:31 +0900

    Add image recognition system [EXPERIMENTAL]
    thanks to karpathy/neuraltalk2

--- chunsabot/modules/images.py
+++ chunsabot/modules/images.py
@@ -1,4 +1,18 @@
+import os
+import json
+import shutil
+import subprocess
+import string
+import random
+
+from chunsabot.database import Database
 from chunsabot.botlogic import brain
+
+RNN_PATH = Database.load_config('rnn_library_path')
+MODEL_PATH = os.path.join(RNN_PATH, "models/checkpoint_v1.t7_cpu.t7")
+
+def id_generator(size=12, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 @brain.route("@image")
 def add_image_description(msg, extras):
@@ -6,4 +20,22 @@ def add_image_description(msg, extras):
     if not attachment:
         return None
 
-    return "asdf"
+    path = os.path.join(brain.__temppath__, id_generator(), 'image_processing')
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    # Moving to temp path
+    img = shutil.move(attachment, path)
+    img_folder = os.path.dirname(img)
+
+    result = subprocess.run(
+        "th {}/eval.lua -model {} -gpuid -1 -image_folder {} -batch_size 1"\
+        .format(RNN_PATH, MODEL_PATH, img_folder)
+    )
+    os.rmdir(img_folder)
+
+    result_message = None
+    with open(os.path.join(result, "vis/vis.json"), 'r') as output:
+        json_output = json.loads(output)
+        result_message = json_output[0]['caption']
+
+    return result_message
